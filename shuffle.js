5801637f13271a4d7121a67a7a6f31fcb33c09b5
Taylor Sandusky <tsandusky@mills.edu>
2018-02-23 17:35:36 -0800

    Improve shuffle

--- shuffle.js
+++ shuffle.js
@@ -1,31 +1,29 @@
 var iTunes = Application("iTunes");
 var library = iTunes.playlists["Library"];
 var sourcePlaylistName = "random helper";
-var searchText = "Hayley";
+var searchText = "bastille";
 var searchSpot = "artists";
 
 var songs = iTunes.search(library, {
 	for: searchText, 
 	only: searchSpot
 });
+var numSongs = songs.length;
 	
-//TODO: check for that playlist first, if there is just update it
+//TODO: check for that playlist first, if it exists just update it
+//create new playlist
 var playlistName = "smart " + searchText
 var newPlaylist = iTunes.UserPlaylist().make();
 newPlaylist.name = playlistName;
 
-var numSongs = songs.length;
-
-//get "random" songs from a good playlist
-var randomPlaylist = iTunes.playlists[sourcePlaylistName];
-var randomSongs = randomPlaylist.tracks;
-
+//get random songs from a good playlist and alternate
+var randomSongs = iTunes.playlists[sourcePlaylistName].tracks;
 var random = false;
 var i = 0;
 while (i < numSongs) {
 	var song;
 	if (random) {
-		song = randomSongs[i];
+		song = randomSongs[Math.floor(Math.random() * randomSongs.length)];
 		random = false;
 		i++;
 	} else {
